3c33c97bcfc9ff5345a99990182de8b850febef4
Mihael Zsivics <zsivics.mihael@gmail.com>
2021-10-20 09:10:42 +0200

    updated readme, version and started working on edit

--- src/components/MyLinkCard.vue
+++ src/components/MyLinkCard.vue
@@ -2,22 +2,50 @@
 <div>
     <div class="container">
         <div class="btnContainer">
-            <button class="removeBtn" @click="removeLinkCard">Remove</button>
+            <div v-if="!isEditMode">
+                <button class="editBtn" @click="editLinkCard">Edit</button>
+                <button class="removeBtn" @click="removeLinkCard">Remove</button>
+            </div>
+            <div v-else>
+                <button class="saveBtn" @click="isEditMode = !isEditMode" >Save</button>
+                <button class="cancelBtn" @click="isEditMode = !isEditMode">Cancel</button>
+            </div>
         </div>
-        <div clas="title">
-            <h3 class="text-xl">{{ myLink.title }}</h3>
+        <div v-if="!isEditMode" class="readFields">
+            <div clas="title">
+                <h3 class="text-xl">{{ myLink.title }}</h3>
+            </div>
+            <div class="url">
+                <a target="_blank" :href=myLink.url>{{ myLink.url }}</a>
+            </div>
+            <div class="description">
+                <p>{{ myLink.description }}</p>
+            </div>
+            <div class="category">
+                <p>{{ myLink.category }}</p>
+            </div>
+            <div class="date">
+                <p>{{ formattedDate }}</p>
+            </div>
         </div>
-        <div class="url">
-            <a target="_blank" :href=myLink.url>{{ myLink.url }}</a>
-        </div>
-        <div class="description">
-            <p>{{ myLink.description }}</p>
-        </div>
-        <div class="category">
-            <p>{{ myLink.category }}</p>
-        </div>
-        <div class="date">
-            <p>{{ formattedDate }}</p>
+        <div v-else class="editFields">
+            <form action="">
+                <div clas="title">
+                    <input type="text">{{ myLink.title }}
+                </div>
+                <div class="url">
+                    <input type="url">{{ myLink.url }}
+                </div>
+                <div class="description">
+                    <input type="text">{{ myLink.description }}
+                </div>
+                <div class="category">
+                    <input type="text">{{ myLink.category }}
+                </div>
+                <div class="date">
+                    <input type="date">{{ formattedDate }}
+                </div>
+            </form>
         </div>
     </div>
 </div>
@@ -30,6 +58,11 @@ export default {
     name: "MyLinkCard",
     emits: ["fetch-links"],
     props: [ "myLink" ],
+    data() {
+        return {
+            isEditMode: false
+        }
+    },
     computed: {
         formattedDate() {
             let dateParts = this.myLink.date.split("T");
@@ -41,6 +74,9 @@ export default {
         async removeLinkCard() {
             await new RequestHandler().removeLinkCard(this.myLink._id);
             this.$emit("fetch-links");
+        },
+        editLinkCard() {
+            this.isEditMode = true;
         }
     }
 }
